14a72b4764a2c71b832e52d5d798f1049698e469
marcyky <112631747+marcyky@users.noreply.github.com>
2022-10-27 09:27:46 -0300

    Add files via upload

---
+++ coisa.py
@@ -0,0 +1,9 @@
+idades = [20,34,15,8,16,18,41,22]
+
+cont = int()
+
+for x in idades:
+    if (x >=18):
+        cont +=1
+        
+print(cont)
---
+++ coisa2.py
@@ -0,0 +1,7 @@
+idades = []
+
+for idade in range(5):
+    idade = int(input('Digite uma idade: '))
+    idades.append(idade)
+    
+print (idades[:2])
